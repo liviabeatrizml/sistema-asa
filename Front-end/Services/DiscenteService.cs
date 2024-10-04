18ea6af25e7e7007b47dd52218cea7a7e3fe068f
Renan Costa <105220100+RenanCosta2@users.noreply.github.com>
2024-10-03 22:01:48 -0300

    <refactor>: ajustes na obrigatoriedade

--- Front-end/Services/DiscenteService.cs
+++ Front-end/Services/DiscenteService.cs
@@ -40,17 +40,18 @@ public class DiscenteRegistroModel
     [Required(ErrorMessage = "O campo Matrícula é obrigatório.")]
     public int Matricula { get; set; }
 
-    public int Telefone { get; set; }
+    public int? Telefone { get; set; }
 
     [Required(ErrorMessage = "O campo CPF é obrigatório.")]
     public string Cpf { get; set; }
 
-    [Required(ErrorMessage = "O campo Curso é obrigatório.")]
-    public string Curso { get; set; }
+    public string? Curso { get; set; }
 }
 
 public class DiscenteLoginModel
 {
+    [Required(ErrorMessage = "O campo Email é obrigatório.")]
     public string Email { get; set; }
+    [Required(ErrorMessage = "O campo Senha é obrigatório.")]
     public string Senha { get; set; }
 }
