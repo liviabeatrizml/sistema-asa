ee9073bf5bc97e871041ba772bf278bad30cbfd3
Renan Costa <105220100+RenanCosta2@users.noreply.github.com>
2024-10-03 22:24:33 -0300

    <feat>: arquivo js que realiza o drag scroll

---
+++ Front-end/wwwroot/js/scroll.js
@@ -0,0 +1,33 @@
+function enableDragScroll(containerId) {
+    const container = document.getElementById(containerId);
+    if (!container) return;
+
+    let isDown = false;
+    let startX;
+    let scrollLeft;
+
+    container.addEventListener('mousedown', (e) => {
+        isDown = true;
+        container.classList.add('active');
+        startX = e.pageX - container.offsetLeft;
+        scrollLeft = container.scrollLeft;
+    });
+
+    container.addEventListener('mouseleave', () => {
+        isDown = false;
+        container.classList.remove('active');
+    });
+
+    container.addEventListener('mouseup', () => {
+        isDown = false;
+        container.classList.remove('active');
+    });
+
+    container.addEventListener('mousemove', (e) => {
+        if (!isDown) return;
+        e.preventDefault();
+        const x = e.pageX - container.offsetLeft;
+        const walk = (x - startX) * 1; // Quanto mais alto o número, maior a velocidade
+        container.scrollLeft = scrollLeft - walk;
+    });
+}
