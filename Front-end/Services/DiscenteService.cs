e0771453092792c2cfeb8294b98ff4ce3bd2fd31
Renan Costa <105220100+RenanCosta2@users.noreply.github.com>
2024-10-11 11:42:36 -0300

    <feat>: método que usa a API de buscar discente

--- Front-end/Services/DiscenteService.cs
+++ Front-end/Services/DiscenteService.cs
@@ -2,6 +2,7 @@ using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json;
 
 public class DiscenteService
 {
@@ -23,6 +24,33 @@ public class DiscenteService
     {
         return await _httpClient.PostAsJsonAsync("/api/Discente/login", model);
     }
+
+    // Método para obter discente
+    public async Task<DiscenteRegistroModel> GetDiscente(int id_discente)
+{
+    // Fazendo a requisição GET
+    var response = await _httpClient.GetAsync($"/api/Discente/obter-discente/{id_discente}");
+
+    // Garantindo que a requisição foi bem-sucedida
+    if (response.IsSuccessStatusCode)
+    {
+        // Lendo o conteúdo da resposta como string
+        var jsonResponse = await response.Content.ReadAsStringAsync();
+        
+        // Desserializando o JSON na classe Discente
+        var discente = JsonSerializer.Deserialize<DiscenteRegistroModel>(jsonResponse, new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true // Ignora a diferença de maiúsculas e minúsculas
+        });
+        
+        return discente;
+    }
+    else
+    {
+        // Lidar com erros, se necessário
+        throw new Exception("Falha ao obter os dados do discente.");
+    }
+}
 }
 
 // Modelos de dados
